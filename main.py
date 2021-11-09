47c44c4c84ae3d2a805ca17d8de8f22bb4cb5889
javierpb275 <javierpb275@gmail.com>
2021-11-09 20:08:51 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import utility
+import shopping.shopping_card
+
+print(utility)  # <module 'utility' from 'C:\\Users\\javie\\Desktop\\programming-courses\\python-course\\python-modules\\utility.py'>
+
+utility.divide(4, 2)
+utility.multiply(2, 2)
+
+print(shopping.shopping_card)# <module 'shopping.shopping_card' from 'C:\\Users\\javie\\Desktop\\programming-courses\\python-course\\python-modules\\shopping\\shopping_card.py'>
+
+shopping.shopping_card.buy('apple')
