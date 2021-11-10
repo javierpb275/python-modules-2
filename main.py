da93d7a6dbbb5dfabfcfc420e98915dfb06cbbc9
javierpb275 <javierpb275@gmail.com>
2021-11-10 18:42:08 +0100

    built-in modules

--- main.py
+++ main.py
@@ -2,10 +2,16 @@
 from utility import multiply, divide
 from shopping.more_shopping import shopping_cart
 print(__name__)#__main__
+#import random as m_random
+from random import shuffle
 
 if __name__ == '__main__':
+    my_list = [1,2,3,4,5]
+    shuffle(my_list)
+    print(my_list)
+    '''
     print(divide(4, 2))
     print(multiply(2, 2))
     print(shopping_cart.buy('apple'))
-
     print(max([1, 2, 3]))
+    '''
