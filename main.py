8705344cfed980503483f90994cf22aa7de1cf97
javierpb275 <javierpb275@gmail.com>
2021-11-10 19:02:04 +0100

    built-in modules 2. sys.argv[] allows us to access the arguments that we write in the terminal

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ from shopping.more_shopping import shopping_cart
 print(__name__)#__main__
 #import random as m_random
 from random import shuffle
+import sys
 
 if __name__ == '__main__':
+    first_name = sys.argv[1]
+    last_name = sys.argv[2]
+    print(f'hello {first_name} {last_name}')
+    '''
     my_list = [1,2,3,4,5]
     shuffle(my_list)
     print(my_list)
-    '''
     print(divide(4, 2))
     print(multiply(2, 2))
     print(shopping_cart.buy('apple'))
