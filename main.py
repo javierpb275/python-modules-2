d53d848122f129496bf6cca94aa4ec8336a3d7db
javierpb275 <javierpb275@gmail.com>
2021-11-10 18:15:51 +0100

    renamed shopping_cart.py

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
 #from utility import *
 from utility import multiply, divide
-from shopping.more_shopping import shopping_card
+from shopping.more_shopping import shopping_cart
 print(__name__)#__main__
 
 if __name__ == '__main__':
     print(divide(4, 2))
     print(multiply(2, 2))
-    print(shopping_card.buy('apple'))
+    print(shopping_cart.buy('apple'))
 
     print(max([1, 2, 3]))
